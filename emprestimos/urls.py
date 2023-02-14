6583d8558cb6d1a502a226253b2e13312edb79af
apolo <apoloc43@icloud.com>
2023-02-14 16:00:51 -0300

    terceiro commit

--- emprestimos/urls.py
+++ emprestimos/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import  path
+from .views import listar_livros,detalhar_livro
 
-from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    
+    path('listar_livros/', listar_livros,name='listar_livros'),
+    path('detalhar_livro/<int:pk>', detalhar_livro,name='detalhar_livro'),
+    
+    
+    
 ]
--- emprestimos/views.py
+++ emprestimos/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Livro
 
 
 # Create your views here.
 
+def listar_livros(request):
+    livros = Livro.objects.all()
+    return render(request, 'listar_livros.html', {'livros':livros})
+
+
+def detalhar_livro(request,pk):
+    livro = Livro.objects.get(pk=pk)
+    return render(request, 'detalhar_livro.html', {'livro':livro})
+    
